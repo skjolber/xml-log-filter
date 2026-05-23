c2fa31a60b669fa5aee9e594e9a8ae4d02b6a944
Thomas Skjølberg <thomas.skjolberg@gmail.com>
2026-05-24 00:24:59 +0200

    Improve build

--- impl/core/src/main/moditech/module-info.java
+++ impl/core/src/main/moditech/module-info.java
@@ -2,8 +2,7 @@ module com.github.skjolber.xmlfilter.core {
 	requires com.github.skjolber.xmlfilter;
 	requires com.github.skjolber.xmlfilter.base;
 	
-	exports com.github.skjolber.xmlfilter.core.DefaultXmlFilterFactory;
-	exports com.github.skjolber.xmlfilter.core.XmlFilterBuilder;
+	exports com.github.skjolber.xmlfilter.core;
 	
     provides com.github.skjolber.xmlfilter.XmlFilterFactory with com.github.skjolber.xmlfilter.core.DefaultXmlFilterFactory;
 }
