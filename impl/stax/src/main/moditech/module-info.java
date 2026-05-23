f41aa41539727b2ea21af65c8b31a264a94c9aef
Thomas Skjølberg <thomas.skjolberg@gmail.com>
2026-05-24 00:38:09 +0200

    add module info

---
+++ impl/stax/src/main/moditech/module-info.java
@@ -0,0 +1,6 @@
+module com.github.skjolber.xmlfilter.stax {
+	requires com.github.skjolber.xmlfilter;
+	requires com.github.skjolber.xmlfilter.base;
+
+	exports com.github.skjolber.xmlfilter.stax;
+}
